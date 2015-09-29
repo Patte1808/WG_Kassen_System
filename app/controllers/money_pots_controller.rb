7dd6d634258c9cd3b25e1ecbf6359720e1c5819a
Patrick Voß <Patte@Patricks-MacBook-Air.local>
2015-09-29 18:23:04 +0200

    Add simple form gem

--- app/controllers/money_pots_controller.rb
+++ app/controllers/money_pots_controller.rb
@@ -2,4 +2,8 @@ class MoneyPotsController < ApplicationController
   def index
     @money_pots = MoneyPot.where(isActive: true).order("created_at ASC")
   end
+
+  def new
+    @money_pot = MoneyPot.new
+  end
 end
