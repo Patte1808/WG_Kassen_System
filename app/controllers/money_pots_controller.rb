377aa1af75e4a28f806e8c8a957f754c3d2c4bef
Patrick Voß <Patte@Patricks-MacBook-Air.local>
2015-09-29 18:28:49 +0200

    Add new / create for moneypot

--- app/controllers/money_pots_controller.rb
+++ app/controllers/money_pots_controller.rb
@@ -6,4 +6,20 @@ class MoneyPotsController < ApplicationController
   def new
     @money_pot = MoneyPot.new
   end
+
+  def create
+    @money_pot = MoneyPot.new(money_pot_params)
+
+    if @money_pot.save
+      redirect_to @money_pot
+    else
+      flash.now[:error] = "Could not save money pot"
+      render action: "new"
+    end
+  end
+
+  private
+  def money_pot_params
+    params.require(:money_pot).permit(:title, :value)
+  end
 end
